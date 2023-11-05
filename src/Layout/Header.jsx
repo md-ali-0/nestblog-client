d4eeaa0410098cdeed3817d666e5aca076c7ddd2
md-ali-0 <mohammad.98482@gmail.com>
2023-11-05 13:36:22 +0600

    A Navbar Section Added

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,8 +1,80 @@
+import {
+    Avatar,
+    Button,
+    DarkThemeToggle,
+    Dropdown,
+    Flowbite,
+    Navbar,
+} from 'flowbite-react';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 const Header = () => {
+    const user = true;
     return (
-        <header>
-            Header
-        </header>
+        <Flowbite>
+            <header>
+                <Navbar
+                    className="bg-white/60 dark:bg-[#161819] shadow-sm backdrop-blur"
+                    container="true"
+                    >
+                    <Link className="flex items-center" to="/">
+                        <img
+                            src={logo}
+                            className="mr-3 h-6 md:h-9 dark:invert"
+                            alt="Flowbite React Logo"
+                        />
+                    </Link>
+                    <div className="flex md:order-2">
+                        <DarkThemeToggle className='mr-2'/>
+                        {user ? (
+                            <Dropdown
+                                arrowIcon={false}
+                                inline
+                                label={
+                                    <Avatar
+                                        alt="User settings"
+                                        img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                                        rounded
+                                    />
+                                }>
+                                <Dropdown.Header>
+                                    <span className="block text-sm">
+                                        Bonnie Green
+                                    </span>
+                                    <span className="block truncate text-sm font-medium">
+                                        name@flowbite.com
+                                    </span>
+                                </Dropdown.Header>
+                                <Dropdown.Item>Dashboard</Dropdown.Item>
+                                <Dropdown.Item>Settings</Dropdown.Item>
+                                <Dropdown.Item>Earnings</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>Sign out</Dropdown.Item>
+                            </Dropdown>
+                        ) : (
+                            <div className="flex gap-3 px-2">
+                                <Button size="sm" color="blue">Login</Button>
+                                <Button size="sm" color="gray">Register</Button>
+                            </div>
+                        )}
+                        <Navbar.Toggle />
+                    </div>
+                    <Navbar.Collapse>
+                        <NavLink
+                            to="/"
+                            className="block py-2 pr-4 pl-3 md:p-0 bg-cyan-700 text-white dark:text-white md:bg-transparent md:text-cyan-700">
+                            Home
+                        </NavLink>
+                        <Navbar.Link href="/blogs">All blogs</Navbar.Link>
+                        <Navbar.Link href="/featured-blogs">
+                            Featured Blogs
+                        </Navbar.Link>
+                        <Navbar.Link href="/about">About</Navbar.Link>
+                        <Navbar.Link href="/contact">Contact</Navbar.Link>
+                    </Navbar.Collapse>
+                </Navbar>
+            </header>
+        </Flowbite>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('flowbite/plugin')],
-}
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        extend: {
+          colors:{
+            'primary': '#0057ff',
+            'body': '#F9F9FF',
+          }
+        },
+    },
+    plugins: [require('flowbite/plugin')],
+};
