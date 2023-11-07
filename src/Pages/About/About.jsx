3f18eff4215cfd5a9f6f182989142c85fc924a96
md-ali-0 <mohammad.98482@gmail.com>
2023-11-07 12:27:17 +0600

    About Content Changed

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
@@ -38,24 +38,27 @@ const About = () => {
                     <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
                         <div className="mt-10 lg:mt-0">
                             <span className="block mb-4 text-lg font-semibold text-primary">
-                                Why Choose Us
+                                Why Our Blog Stands Out
                             </span>
                             <h2 className="mb-5 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
-                                Make your customers happy by giving services.
+                                Elevate Customer Satisfaction with Our Insights
                             </h2>
                             <p className="mb-5 text-base text-body-color dark:text-dark-6">
-                                It is a long established fact that a reader will
-                                be distracted by the readable content of a page
-                                when looking at its layout. The point of using
-                                Lorem Ipsum is that it has a more-or-less.
+                                Our blog delivers valuable insights that
+                                captivate and engage readers. When crafting your
+                                content, it&apos;s essential to ensure it captures
+                                your audience&apos;s attention and provides relevant
+                                information.
                             </p>
                             <p className="mb-8 text-base text-body-color dark:text-dark-6">
-                                A domain name is one of the first steps to
-                                establishing your brand. Secure a consistent
-                                brand image with a domain name that matches your
-                                business.
+                                The choice of a domain name is a critical first
+                                step in building your brand. Make a lasting
+                                impression with a domain name that aligns
+                                perfectly with your business identity.
                             </p>
-                            <Link to='/'
+
+                            <Link
+                                to="/"
                                 className="inline-flex items-center justify-center py-3 text-base font-medium text-center text-white border border-transparent rounded-md px-7 bg-primary hover:bg-opacity-90">
                                 Get Started
                             </Link>
